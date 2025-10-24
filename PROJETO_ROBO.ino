376599df09cf8a540941704d1de2e6baf80ace26
julialenzgoetz <112575677+julialenzgoetz@users.noreply.github.com>
2025-10-24 22:41:08 +0000

    atualizandoyiiiiiii

--- PROJETO_ROBO.ino
+++ PROJETO_ROBO.ino
@@ -3,34 +3,44 @@ PROFESSOR: JEAN SCHMITH
 ALUNOS: JULIA LENZ GOETZ E LUCAS SCHERER
 PROJETO GRAU B - CARRINHO*/
 
-//Estados da Maquina de Estados
-#define SEGUIR_LINHA 0
-#define CURVA 1
-#define PARADA 2
+//------------------------------ Estados e Direções -----------------------------------------------
+enum ESTADO   : uint8_t { SEGUIR_LINHA=0, CURVA=1, PARADA=2 };
+enum DIRECAO  : uint8_t { NENHUMA=0, ESQUERDA=1, DIREITA=2 };
 
-//Definição da Velocidade
-#define VELOCIDADE_DIREITA 50
-#define VELOCIDADE_ESQUERDA 50
-#define VELOCIDADE_CURVA 50
+//------------------------------ Definição da Velocidade (0-255) ---------------------------------
+#define VEL_ESQUERDA_BASE 200    
+#define VEL_DIREITA_BASE 180
+#define VEL_CURVA_ESQ 100
+#define VEL_CURVA_DIR 100
+#define CURVA_MS 400 //duração da curva de 90° em ms
 
-//Pinos
+//------------------------------ Definição de Pinos ----------------------------------------------
 #define LED_DEBUG 13
-#define IR_CONTADOR_LINHA 3
-#define IR_OBSTACULO 2
-#define IR_SEGUIDOR_DIREITO 11
-#define IR_SEGUIDOR_ESQUERDO 12
-#define PWM_MOTOR_DIREITO 5
-#define PWM_MOTOR_ESQUERDO 6
+#define IR_CONTADOR_LINHA 3   //interrupção
+#define IR_OBSTACULO 2        //interrupção
+#define IR_SEGUIDOR_DIREITO 11   //digital  
+#define IR_SEGUIDOR_ESQUERDO 12  //digital
+
+#define PWM_MOTOR_DIREITO 5     //PWM
+#define PWM_MOTOR_ESQUERDO 6    //PWM
 #define MOTOR_DIREITO_IN_0 8
 #define MOTOR_DIREITO_IN_1 7
 #define MOTOR_ESQUERDO_IN_0 10
 #define MOTOR_ESQUERDO_IN_1 9
 
-//Variaveis globais
-int estado = SEGUIR_LINHA;
+//------------------------------ Variaveis Globais ----------------------------------------------
+ESTADO estado = SEGUIR_LINHA;
+DIRECAO direcao = NENHUMA;
 int iCount = 0;
-int isObstaculo = 0;
+int isObstaculo = 0; // Logo, 1 = obstáculo presente.
+int SENSOR_ESQUERDO = 0;
+int SENSOR_DIREITO = 0;
 
+const uint8_t COR_BRANCA = 0;
+const uint8_t COR_PRETA  = 1;
+
+
+//----------------------------------- SETUP -----------------------------------------------------
 void setup()
 {
   //Configuração dos Pinos
@@ -50,6 +60,7 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(IR_CONTADOR_LINHA), contador_linha, RISING);
 }
 
+//----------------------------- MÁQUINA DE ESTADOS ------------------------------------------------
 void loop()
 {
   switch(estado)
@@ -60,10 +71,10 @@ void loop()
 
     case CURVA:
       if (direcao == ESQUERDA) {
-        curva_esquerda();
+        curvar_esquerda();
       }
       else if (direcao == DIREITA) {
-        curva_direita();
+        curvar_direita();
       }
       break;
 
@@ -73,68 +84,59 @@ void loop()
   }
 }
 
-void andar_para_frente() 
+// ---------------------------- AÇÕES DE MOVIMENTO --------------------------------------------------
+void andar_frente() 
 {
-  // Configura ambos os motores no mesmo sentido → carrinho anda reto
-  // Faixa de velocidade 0-255
+  // Configura ambos os motores no mesmo sentido → carrinho anda reto para frente
 
-  // Motor Direito
-  digitalWrite(MOTOR_DIREITO_IN_0, HIGH);
-  digitalWrite(MOTOR_DIREITO_IN_1, LOW);
-  analogWrite(PWM_MOTOR_DIREITO, VELOCIDADE); 
-
-  // Motor Esquerdo
-  digitalWrite(MOTOR_ESQUERDO_IN_0, HIGH);
-  digitalWrite(MOTOR_ESQUERDO_IN_1, LOW);
-  analogWrite(PWM_MOTOR_ESQUERDO, VELOCIDADE); 
-}
-
-void seguidor_de_linha()
-{
-  // Neste processo, armazenamos o valor lido pelo sensor na variável SENSOR.
-  int SENSOR_ESQUERDO = digitalRead(IR_SEGUIDOR_ESQUERDO);
-  int SENSOR_DIREITO = digitalRead(IR_SEGUIDOR_DIREITO);
-
-  //Para a cor branca atribuímos o valor 0 e, para a cor preta, o valor 1.
-  if (SENSOR_ESQUERDO == 0 && SENSOR_DIREITO == 1) {
-    //Autocorreção para direita
-    estado == CURVA_DIREITA
-  }
-  else if (SENSOR_ESQUERDO == 1 && SENSOR_DIREITO == 0) {
-  // Autocorreção para esquerda
-    estado = CURVA_ESQUERDA; 
-  }
-  else {
-  andar_para_frente()
-  }
-}
-
-void esquerda() {
-  // Motor Direito para frente
-  digitalWrite(MOTOR_DIREITO_IN_0, HIGH);
-  digitalWrite(MOTOR_DIREITO_IN_1, LOW);
-  analogWrite(PWM_MOTOR_DIREITO, VELOCIDADE); 
-  // Motor Esquerdo para trás
+  // Esquerdo
   digitalWrite(MOTOR_ESQUERDO_IN_0, LOW);
   digitalWrite(MOTOR_ESQUERDO_IN_1, HIGH);
-  analogWrite(PWM_MOTOR_ESQUERDO, VELOCIDADE); 
-}
-
-void direita() {
-  // Motor Direito para trás
+  analogWrite(PWM_MOTOR_ESQUERDO, VEL_ESQUERDA_BASE);
+  // Direito
   digitalWrite(MOTOR_DIREITO_IN_0, LOW);
   digitalWrite(MOTOR_DIREITO_IN_1, HIGH);
-  analogWrite(PWM_MOTOR_DIREITO, VELOCIDADE_CURVA); 
-  // Motor Esquerdo para frente
-  digitalWrite(MOTOR_ESQUERDO_IN_0, HIGH);
-  digitalWrite(MOTOR_ESQUERDO_IN_1, LOW);
-  analogWrite(PWM_MOTOR_ESQUERDO, VELOCIDADE_CURVA); 
+  analogWrite(PWM_MOTOR_DIREITO, VEL_DIREITA_BASE);
 }
 
-void realizar_a_curva()
+void andar_tras() 
 {
-  //Fazer a curva
-  estado = SEGUIR_LINHA;
+  // Configura ambos os motores no mesmo sentido → carrinho anda reto para trás
+
+  // Esquerdo
+  digitalWrite(MOTOR_ESQUERDO_IN_0, HIGH);
+  digitalWrite(MOTOR_ESQUERDO_IN_1, LOW);
+  analogWrite(PWM_MOTOR_ESQUERDO, VEL_ESQUERDA_BASE);
+  // Direito
+  digitalWrite(MOTOR_DIREITO_IN_0, HIGH);
+  digitalWrite(MOTOR_DIREITO_IN_1, LOW);
+  analogWrite(PWM_MOTOR_DIREITO, VEL_DIREITA_BASE);
+}
+
+void curvar_esquerda() {
+  // curva fechada: gira no eixo (E trás, D frente)
+
+  // Esquerdo
+  digitalWrite(MOTOR_ESQUERDO_IN_0, HIGH);
+  digitalWrite(MOTOR_ESQUERDO_IN_1, LOW);
+  analogWrite(PWM_MOTOR_ESQUERDO, VEL_ESQUERDA_BASE);
+  // Direito
+  digitalWrite(MOTOR_DIREITO_IN_0, LOW);
+  digitalWrite(MOTOR_DIREITO_IN_1, HIGH);
+  analogWrite(PWM_MOTOR_DIREITO, VEL_DIREITA_BASE);
+}
+
+void curvar_direita() {
+  // curva fechada: gira no eixo (E frente, D trás)
+
+  // Esquerdo
+  digitalWrite(MOTOR_ESQUERDO_IN_0, LOW);
+  digitalWrite(MOTOR_ESQUERDO_IN_1, HIGH);
+  analogWrite(PWM_MOTOR_ESQUERDO, VEL_ESQUERDA_BASE);
+  // Direito
+  digitalWrite(MOTOR_DIREITO_IN_0, HIGH);
+  digitalWrite(MOTOR_DIREITO_IN_1, LOW);
+  analogWrite(PWM_MOTOR_DIREITO, VEL_DIREITA_BASE);
 }
 
 void parar()
@@ -147,6 +149,48 @@ void parar()
   digitalWrite(MOTOR_ESQUERDO_IN_0, LOW);
   digitalWrite(MOTOR_ESQUERDO_IN_1, LOW);
 }
+
+//---------------------------- LEITURA DOS SENSORES DE LINHA ---------------------------------------
+
+// Neste processo, armazenamos o valor lido pelo sensor na variável SENSOR.
+void lerSensoresLinha(void) {
+  SENSOR_ESQUERDO = digitalRead(IR_SEGUIDOR_ESQUERDO);
+  SENSOR_DIREITO = digitalRead(IR_SEGUIDOR_DIREITO);
+}
+
+// ----------------------------- SEGUIDOR DE LINHA --------------------------------------------------
+void seguidor_de_linha()
+{
+  lerSensoresLinha();
+  if (SENSOR_ESQUERDO == COR_PRETA && SENSOR_DIREITO == COR_BRANCA) {
+    // linha à esquerda → autocorreção à esquerda (curva suave)
+    estado = CURVA;
+    direcao = ESQUERDA;
+  }
+  
+  else if (SENSOR_ESQUERDO == COR_BRANCA && SENSOR_DIREITO == COR_PRETA) {
+    // linha à direita → autocorreção à direita (curva suave)
+    estado = CURVA;
+    direcao = DIREITA;
+  } 
+  
+  else if (SENSOR_ESQUERDO == COR_PRETA && SENSOR_DIREITO == COR_PRETA) {
+    // cruzamento / faixa cheia → avança simétrico
+    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+  } else {
+    // ambos em branco → segue reto
+    andar_reto();
+  }
+  
+}
+
+void realizar_a_curva()
+{
+  //Fazer a curva
+  estado = SEGUIR_LINHA;
+}
+
 
 void contador_linha()
 {
